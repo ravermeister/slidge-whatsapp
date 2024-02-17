c745aa4050441aaa2966c0f2dfa43f9cc5513a86
nicoco <nicoco@nicoco.fr>
2024-02-17 14:49:38 +0100

    chore: update entrypoint

--- slidge_whatsapp/__main__.py
+++ slidge_whatsapp/__main__.py
@@ -1,11 +1,8 @@
-import sys
-
-from slidge.__main__ import main as slidge_main
+from slidge import entrypoint
 
 
-def main() -> None:
-    sys.argv.extend(["--legacy", "slidge_whatsapp"])
-    slidge_main()
+def main():
+    entrypoint("slidge_whatsapp")
 
 
 if __name__ == "__main__":
