e3c36becdcaebe9d02b022acb91bbc2366666583
nicoco <nicoco@nicoco.fr>
2024-02-17 14:51:10 +0100

    feat: group creation and affiliation changes

--- slidge_whatsapp/session.go
+++ slidge_whatsapp/session.go
@@ -453,6 +453,32 @@ func (s *Session) GetGroups() ([]Group, error) {
 	return groups, nil
 }
 
+// CreateGroup attempts to create a new WhatsApp group for the given human-readable name and
+// participant JIDs given.
+func (s *Session) CreateGroup(name string, participants []string) (Group, error) {
+	if s.client == nil || s.client.Store.ID == nil {
+		return Group{}, fmt.Errorf("Cannot create group for unauthenticated session")
+	}
+
+	var jids []types.JID
+	for _, p := range participants {
+		jid, err := types.ParseJID(p)
+		if err != nil {
+			return Group{}, fmt.Errorf("Could not parse participant JID: %s", err)
+		}
+
+		jids = append(jids, jid)
+	}
+
+	req := whatsmeow.ReqCreateGroup{Name: name, Participants: jids}
+	info, err := s.client.CreateGroup(req)
+	if err != nil {
+		return Group{}, fmt.Errorf("Could not create group: %s", err)
+	}
+
+	return newGroup(s.client, info), nil
+}
+
 // GetAvatar fetches a profile picture for the Contact or Group JID given. If a non-empty `avatarID`
 // is also given, GetAvatar will return an empty [Avatar] instance with no error if the remote state
 // for the given ID has not changed.
@@ -541,6 +567,23 @@ func (s *Session) SetGroupTopic(resourceID, topic string) error {
 	}
 
 	return s.client.SetGroupTopic(jid, "", "", topic)
+}
+
+func (s *Session) SetAffiliation(groupID, participantID string, change whatsmeow.ParticipantChange) ([]types.GroupParticipant, error) {
+	if s.client == nil || s.client.Store.ID == nil {
+		return make([]types.GroupParticipant, 0), fmt.Errorf("Cannot set affiliation for unauthenticated session")
+	}
+
+	groupJID, err := types.ParseJID(groupID)
+	if err != nil {
+		return make([]types.GroupParticipant, 0), fmt.Errorf("Could not parse JID for affiliation change: %s", err)
+	}
+
+	participantJID, err := types.ParseJID(participantID)
+	if err != nil {
+		return make([]types.GroupParticipant, 0), fmt.Errorf("Could not parse JID for affiliation change: %s", err)
+	}
+	return s.client.UpdateGroupParticipants(groupJID, []types.JID{participantJID}, change)
 }
 
 // FindContact attempts to check for a registered contact on WhatsApp corresponding to the given
