31ec8e6e8828b1661252eb141d799801bfb645bf
Alex Palaistras <alex@deuill.org>
2024-11-12 00:14:32 +0000

    Really fix location message processing
    This commit fixes issues present in previous commits, which broke
    message processing in general, and didn't fix underlying issues with
    location parsing either.

--- slidge_whatsapp/session.py
+++ slidge_whatsapp/session.py
@@ -550,13 +550,12 @@ class Session(BaseSession[str, Recipient]):
         self, message: whatsapp.Message, muc: Optional["MUC"] = None
     ) -> str:
         body = message.Body
-        loc = message.Location
         if muc:
             body = await muc.replace_mentions(body)
-        if loc.Latitude is not None and loc.Longitude is not None:
-            body = "geo:%f,%f" % (loc.Latitude, loc.Longitude)
-            if loc.Accuracy > 0:
-                body = body + ";u=%d" % loc.Accuracy
+        if message.Location.Latitude != 0 or message.Location.Longitude != 0:
+            body = "geo:%f,%f" % (message.Location.Latitude, message.Location.Longitude)
+            if message.Location.Accuracy > 0:
+                body = body + ";u=%d" % message.Location.Accuracy
         if message.IsForwarded:
             body = "↱ Forwarded message:\n " + add_quote_prefix(body)
         return body
